6c9da945f6a0ed85d9979487d5a069ffdafbe894
Caio <ca.ragazzi@gmail.com>
2020-02-22 17:57:53 -0300

    começo projeto

---
+++ src/components/sideBar/SideBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-navigation-drawer v-model="drawer">
+    <v-list>
+      <v-subheader>REPORTS</v-subheader>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false
+    };
+  }
+};
+</script>
+
+<style>
+</style>
